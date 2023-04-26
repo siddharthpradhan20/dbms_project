3379fc4b20ffdc580acea8f274e3068a70f13cce
unknown <siddharthpradhan20@gmail.com>
2023-04-25 21:55:53 -0400

    home_page

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>WOODY'S AUTOMOTIVE STORE</title>
+</head>
+<body>
+
+<h1>WELCOME TO WOODY'S AUTOMOTIVE STORE</h1>
+
+<style>
+  button {
+    background-color: blue;
+    color: white;
+    font-size: 18px;
+    padding: 10px 20px;
+    border-radius: 5px;
+  }
+  
+  button a {
+    text-decoration: none;
+    color: white;
+  }
+</style>
+
+<button>
+  <a href="">EXISTING CUSTOMER?</a>
+</button>
+
+<button>
+  <a href="./customer_registration.php">NEW CUSTOMER?</a>
+</button>
+
+<!-- <button>
+  <a href="https://example.com">ADMIN LOGIN</a>
+</button> -->
+</body>
+</html>
